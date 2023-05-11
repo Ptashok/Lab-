8fdb0a2ee97675d12523f3f9fbf330131cb44a95
Ptashok <133052611+Ptashok@users.noreply.github.com>
2023-05-11 19:17:27 +0200

    Create Lab2.java
    Lab

---
+++ Lab2.java
@@ -0,0 +1,52 @@
+public class Lab2 {
+    public static void main(String[] args) {
+        int n = 3;
+        int[][] a = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] = n*i + j;
+            }
+        }
+        // вивід на екран початкову матрицю
+        System.out.println("Початкова Матриця");
+//        System.out.println("------");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.printf("%3d", a[i][j]);
+            }
+            System.out.println();
+        }
+        // транспонування матриці
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                int temp = a[i][j];
+                a[i][j] = a[j][i];
+                a[j][i] = temp;
+            }
+        }
+        // вивід на екран транспоновану матрицю
+        System.out.println("Нова матриця");
+//        System.out.println("------");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.printf("%3d", a[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println("Мінімальні елементи кожного стовпця");
+//        System.out.println("------");
+        int sum = 0;
+        for (int j = 0; j < a[0].length; j++) {
+            int min = a[0][j];
+            for (int i = 0; i < a.length; i++) {
+                if (a[i][j] < min) {
+                    min = a[i][j];
+                }
+            }
+            System.out.printf("%3d", min);
+            sum += min;
+        }
+        System.out.println();
+        System.out.println("cума = " + sum);
+    }
+}
