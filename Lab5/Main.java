7581fdfb5b35f1ccc7e8f6cff44c8425c12979bd
Ivan Fomyn <133052611+Ptashok@users.noreply.github.com>
2023-05-21 16:22:52 +0200

    Update Main.java

--- Lab5/Main.java
+++ Lab5/Main.java
@@ -2,14 +2,13 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        // Input text
         String inputText = "The sun shines bright, warming our souls! \n" +
                 "Love conquers all, even in darkest times?\n" +
                 "Embrace change, for growth awaits within. ";
         inputText = inputText.replaceAll("\s+", " ");
         Text text = new Text(inputText);
 
-        // Sort words by increasing number of vowels
+        // сортування слів за збільшенням кількості голосних
         List<Word> words = new ArrayList<>();
         for (Sentence sentence : text.getSentences()) {
             words.addAll(Arrays.asList(sentence.getWords()));
