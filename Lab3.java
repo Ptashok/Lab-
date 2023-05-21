b06b5ea6ee307cbc8e4ffff6d213c0078939214c
Ivan Fomyn <133052611+Ptashok@users.noreply.github.com>
2023-05-21 16:24:39 +0200

    Update Lab3.java

--- Lab3.java
+++ Lab3.java
@@ -2,9 +2,9 @@ import java.util.*;
 public class Lab3 {
     public static void main(String[] args) {
         String text = "The sun shines bright, warming our souls! ";
-        // Split the text into individual words
+        // Split the text - розбиття тексту на окремі слова
         String[] words = text.split(" ");
-        // Sort the words based on the number of vowels
+        // слова за кількістю голосних
         Arrays.sort(words, Comparator.comparingInt(Lab3::countVowels));
         // Print the sorted words
         System.out.println("Sorted words:");
